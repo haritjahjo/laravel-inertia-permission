fd5d18fbe6bcc8cc456f9b3df23a870028e213c0
Hari Tjahjo <haritjahjo@gmail.com>
2023-04-09 00:04:18 +0700

    Create Permissions JS

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tightenco\Ziggy\Ziggy;
@@ -32,13 +33,7 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'auth.user' => fn () => $request->user()
-                ? $request->user()->only('id', 'name', 'email')
-                : null,
-            'auth.user.roles' => fn () => $request->user()
-                ? $request->user()->getRoleNames()
-                : null,
-            'auth.user.permissions' => fn () => $request->user()
-                ? $request->user()->getPermissionNames()
+                ? new UserResource($request->user())
                 : null,
             'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy)->toArray(), [
