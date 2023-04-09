c8cab86155ba981d3bbaa3bd2eb1c5619f643cb0
Hari Tjahjo <haritjahjo@gmail.com>
2023-04-09 08:26:32 +0700

    Add Create Role

---
+++ resources/js/Pages/Admin/Roles/Create.vue
@@ -0,0 +1,53 @@
+<script setup>
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+
+const form = useForm({
+    name: '',
+});
+
+// const submit = () => {
+//     form.post(route('store'), {
+//         onFinish: () => form.reset('name'),
+//     });
+// };
+</script>
+
+<template>
+    <Head title="Create a new role" />
+
+    <AdminLayout>
+        <div class="max-w-7xl mx-auto py-4">
+            <div class="flex justify-end">
+                
+                <Link :href="route('roles.index')"
+                    class="px-3 py-2 text-white font-semibold bg-indigo-500 hover:bg-indigo-700 rounded">
+                Back
+                </Link>
+            </div>
+            <div class="mt-6 max-w-md ms-auto">
+                <form @submit.prevent="form.post(route('roles.store'))" >
+                    <div>
+                        <InputLabel for="name" value="name" />
+
+                        <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" autofocus
+                            autocomplete="username" />
+
+                        <InputError class="mt-2" :message="form.errors.name" />
+                    </div>
+
+                    <div class="flex items-center justify-start mt-4">
+                        <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            Create
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </AdminLayout>
+</template>
--- resources/js/Pages/Admin/Roles/RoleIndex.vue
+++ resources/js/Pages/Admin/Roles/RoleIndex.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AdminLayout from '@/Layouts/AdminLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 
 import Table from "@/Components/Table.vue";
 import TableRow from '@/Components/TableRow.vue';
@@ -15,7 +15,14 @@ defineProps(['roles']);
 
     <AdminLayout>
         <div class="max-w-7xl mx-auto py-4">
-            <h1>Roles Index Page!</h1>
+            <div class="flex justify-between">
+                <h1>Roles Index Page!</h1>
+                <Link 
+                    :href="route('roles.create')" 
+                    class="px-3 py-2 text-white font-semibold bg-indigo-500 hover:bg-indigo-700 rounded" >
+                    New Role
+                </Link>
+            </div>
             <div class="mt-6">
                 <Table>
                     <template #header>
